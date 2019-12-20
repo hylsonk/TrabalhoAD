89b9a75e72d654b3b2fc5286dde2e8bae290b28f
Hylson Kobayashi <hylsonk@gmail.com>
2019-12-19 23:03:45 -0300

    Ajustando os simuladores para serem funções

--- fila de banco.R
+++ fila de banco.R
@@ -4,66 +4,50 @@ library(gridExtra)
 
 set.seed(1933)
 
-
-
-
-start <- function(n = 100){
+doubleQueue <- function(n = 1, lambda1 = 1, lambda2 = 1, mu1 = 1, mu2 = 1, priorityQueue = 0){
   i <- 1
-  while(i<10){
-    server <- simmer()
-    
+  while(i<n){
     customer <-
       trajectory("Customer's path") %>%
-      set_attribute("start_time", function() {now(server)}) %>%
-      log_(function() {
-        paste("Queue is", get_queue_count(server, "server"), "on arrival")
-      }) %>%
       seize("server") %>%
-      log_(function() {paste("Waited", now(server) - get_attribute(server, "start_time"))}) %>%
-      set_attribute("start_server", function() {now(server)}) %>%
-      log_(function() {
-        paste("serving", get_attribute(server, "start_server"))
-      }) %>%
-      timeout(function() rexp(1, 1/20)) %>%
-      release("server") %>%
-      log_(function() {paste("served", now(server) - get_attribute(server, "start_server"))}) %>%
-      log_("Completed")
+      timeout(function() rexp(1, mu1)) %>%
+      release("server")
     
     priority <-
       trajectory("Customer's path") %>%
-      set_attribute("start_time", function() {now(server)}) %>%
-      log_(function() {
-        paste("Queue is", get_queue_count(server, "server"), "on arrival")
-      }) %>%
       seize("server") %>%
-      log_(function() {paste("Waited", now(server) - get_attribute(server, "start_time"))}) %>%
-      set_attribute("start_server", function() {now(server)}) %>%
-      log_(function() {
-        paste("serving", get_attribute(server, "start_server"))
-      }) %>%
-      timeout(function() rexp(1, 1/20)) %>%
-      release("server") %>%
-      log_(function() {paste("served", now(server) - get_attribute(server, "start_server"))}) %>%
-      log_("Completed")
+      timeout(function() rexp(1, mu2)) %>%
+      release("server")
     
     server <-
       simmer("server") %>%
       add_resource("server") %>%
-      add_generator("Customer", customer, function() {c(0, rexp(100000, 1/10), -1)}) %>%
-      add_generator("Custome with priority", priority, function() {c(0, rexp(100000, 1/10), -1)}, priority= 1)
-    server %>% run(500)
-    
-    resources <- get_mon_resources(server)
-    arrivals <- get_mon_arrivals(server)
-    
-    # p1=plot(resources, metric = "usage", "counter", items="server", step=TRUE)
-    
-    p2=plot(arrivals, metric = "waiting_time")
-    
-    grid.arrange(p2)
+      add_generator("Customer", customer, function() rexp(1, lambda1)) %>%
+      add_generator("Custome with priority", priority, function() rexp(1, lambda2), priority = priorityQueue) %>%
+      run(500)
     
     i = i+1
   }
 }
 
-start(10)
+singleQueue <- function(n = 1, lambda = 1, mu1 = 1){
+  i <- 1
+  while(i<n){
+    customer <-
+      trajectory("Customer's path") %>%
+      seize("server") %>%
+      timeout(function() rexp(1, mu1)) %>%
+      release("server")
+    
+    server <-
+      simmer("server") %>%
+      add_resource("server") %>%
+      add_generator("Customer", customer, function() rexp(1, lambda1)) %>%
+      run(500)
+    
+    i = i+1
+  }
+}
+
+doubleQueue(1)
+singleQueue(1)
