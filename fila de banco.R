7654730db4f5bdebdb6b558e410f23f1e6f91960
Hylson Kobayashi <hylsonk@gmail.com>
2019-12-20 23:37:57 -0300

    Correção, agora filas não se repetem

--- fila de banco.R
+++ fila de banco.R
@@ -2,28 +2,26 @@ library(simmer)
 library(simmer.plot)
 library(gridExtra)
 
-set.seed(1933)
-
 doubleQueue <- function(n = 1, lambda1 = 1, lambda2 = 1, mu1 = 1, mu2 = 1, priorityQueue = 0){
   i <- 1
   while(i<n){
     customer <-
       trajectory("Customer's path") %>%
       seize("server") %>%
-      timeout(function() c(0,rexp(1, mu1),-1)) %>%
+      timeout(rexp(1, mu1)) %>%
       release("server")
     
     priority <-
       trajectory("Custome with priority's path") %>%
       seize("server") %>%
-      timeout(function() c(0,rexp(1, mu2),-1)) %>%
+      timeout(rexp(1, mu2)) %>%
       release("server")
     
     server <-
       simmer("server") %>%
       add_resource("server") %>%
-      add_generator("Customer", customer, function() c(0,rexp(100000, lambda1),-1)) %>%
-      add_generator("Custome with priority", priority, function() c(0,rexp(100000, lambda2),-1), priority = priorityQueue) %>%
+      add_generator("Customer", customer, function() {rexp(100, lambda1)}) %>%
+      add_generator("Custome with priority", priority, function() {rexp(100, lambda2)}) %>%
       run(100)
     arrivals <- get_mon_arrivals(server)
     return(arrivals)
@@ -51,7 +49,7 @@ singleQueue <- function(n = 1, lambda = 1, mu1 = 1){
   
 }
 
-r <-doubleQueue(n = 10, lambda1 = 0.05, lambda2 = 0.2 , mu1 = 1, mu2 = 0.5, priorityQueue = 0)
+r <-doubleQueue(n = 10, lambda1 = 0.05, lambda2 = 0.2 , mu1 = 1, mu2 = 0.5, priorityQueue = 1)
 
 r
 
